1686725bc86f3c4008d73895e1f7f36a35e064f0
s4nt14go <santiagopergamo@gmail.com>
2020-12-23 13:46:52 -0300

    feat: add cognito.UserPool and cognito.UserPoolClient

--- lib/next-backend-stack.ts
+++ lib/next-backend-stack.ts
@@ -1,9 +1,32 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as cognito from '@aws-cdk/aws-cognito'
+import * as appsync from '@aws-cdk/aws-appsync'
+import * as ddb from '@aws-cdk/aws-dynamodb'
+import * as lambda from '@aws-cdk/aws-lambda'
 
 export class NextBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const userPool = new cognito.UserPool(this, 'cdk-blog-user-pool', {
+      selfSignUpEnabled: true,
+      accountRecovery: cognito.AccountRecovery.PHONE_AND_EMAIL,
+      userVerification: {
+        emailStyle: cognito.VerificationEmailStyle.CODE
+      },
+      autoVerify: {
+        email: true
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true
+        }
+      }
+    })
+
+    const userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
+      userPool
+    })
   }
 }
